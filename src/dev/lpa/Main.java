bb953aa7292f953b7b0fb8b26b6dbc8f53b374ba
Michael Kopp <MichaelJG.Kopp@gmail.com>
2024-09-24 13:53:22 +0200

    add Streamof(e1, e2, ...)

--- src/dev/lpa/Main.java
+++ src/dev/lpa/Main.java
@@ -1,5 +1,6 @@
 package dev.lpa;
 
+import java.util.Arrays;
 import java.util.stream.Stream;
 
 public class Main {
@@ -15,10 +16,28 @@ public class Main {
     var streamI = Stream.iterate(seed, i -> i + 1)
       .limit(15)
       .map(i -> "I" + i);
-    streamI.forEach(System.out::println);
+//    streamI.forEach(System.out::println);
 
     seed += 15;
+    String[] oLabels = new String[15];
+    int seedCopy = seed;
+    Arrays.setAll(oLabels, i -> "N" + (i + seedCopy));
+    var streamN = Arrays.stream(oLabels);
+//    streamN.forEach(System.out::println);
+
     seed += 15;
+//    var streamG = Stream.empty();
+//    for (int i = seed; i < seed + 15; i++) {
+//      streamG = Stream.concat(streamG, Stream.of("G" + i));
+//    }
+    var streamG = Stream.of(
+      "G46", "G47", "G48", "G49", "G50",
+        "G51", "G51", "G53", "G54", "G55",
+        "G56", "G57", "G58", "G59", "G60");
+//    streamG.forEach(System.out::println);
+
     seed += 15;
+
+    var streamBI = Stream.concat(streamB, streamI);
   }
 }
